a3c07a24e89ea3279676823bdf293035f3675119
malini <malini.venkatraman@gmail.com>
2020-08-02 00:49:15 +0530

    removed comments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import NavigationBar from './components/NavigationBar'
 import Row from './components/Row'
-//import Box from '@material-ui/core/Box';
-//import Typography from '@material-ui/core/Typography';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -10,16 +8,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-//import TextField from '@material-ui/core/TextField';
-//import Button from '@material-ui/core/Button';	
-//import { makeStyles } from '@material-ui/core/styles';
 import {Tabs, Nav, Content} from 'react-tiny-tabs';
 import './css/index.css';
-
-//import { sizing, spacing } from '@material-ui/system';
-//import InputBase from '@material-ui/core/InputBase';
-//import IconButton from '@material-ui/core/IconButton';
-//import SearchIcon from '@material-ui/icons/Search';
 import Search from './components/Search'
 
 
